1eceb07d894ee7b94dc71cf94aa9f9180b7ebbc4
surahutomo.pradana@bukalapak.com <surahutomo.pradana@bukalapak.com>
2023-09-06 03:27:27 +0700

    refactor

--- services/AdminService.js
+++ services/AdminService.js
@@ -1,12 +1,22 @@
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
 const Admin = require("../models/admin");
 const Constant = require("../api/Constant");
 
-const passport = require("passport");
-const jwt = require("jsonwebtoken");
-const base64 = require("base-64");
+const checkUserCredentials = (user_id, password) => {
+  const isValidCharacters = /^[a-zA-Z0-9]+$/i.test(user_id);
+  const isValidUserIdLength = user_id.length > 5 && user_id.length < 21;
+  const isValidPasswordLength = password.length > 7 && password.length < 21;
+  const isValidPasswordCharacters =
+    /^[a-z0-9!"#$%&'()*+,.\/:;<=>?@\[\]^_`{|}~-]*$/i.test(password);
+
+  return isValidUserIdLength && isValidCharacters && isValidPasswordLength && isValidPasswordCharacters;
+};
 
 const signUp = async (req, res) => {
   const { user_id, password, nickname, comment } = req.body;
+
   if (!(user_id && password)) {
     const response = {
       message: "Account creation failed",
@@ -14,55 +24,48 @@ const signUp = async (req, res) => {
     };
     return res.status(400).send(response);
   }
-  const isValidCharacters = /^[a-zA-Z0-9]+$/i.test(user_id);
-  const isValidUserIdLength = user_id.length > 5 && user_id.length < 21;
-  const isValidUserId = isValidUserIdLength && isValidCharacters;
-  const isValidPasswordLength = password.length > 7 && user_id.length < 21;
-  const isValidPasswordCharacters =
-    /^[a-z0-9!"#$%&'()*+,.\/:;<=>?@\[\]^_`{|}~-]*$/i.test(password);
 
-  const isValidPassword = isValidPasswordLength && isValidPasswordCharacters;
+  const isValidCredentials = checkUserCredentials(user_id, password);
 
-  if (!(isValidUserId && isValidPassword)) {
+  if (!isValidCredentials) {
     const response = {
       message: "Account creation failed",
-      cause: "required user_id and password",
+      cause: "invalid user_id or password format",
     };
     return res.status(400).send(response);
   }
 
-  // return res.status(200).send({ user_id: validRegex, password: ASCII });
-
   Admin.findOne({ user_id }, (err, result) => {
     if (err) {
       return res.status(500).json({ success: false, error: err });
-    } else {
-      if (!result) {
-        let newUser = new Admin({
-          user_id,
-          password,
-          nickname,
-          comment,
-        });
-        Admin.createAdmin(newUser, (err, user) => {
-          if (err) throw err;
-          const response = {
-            message: "Account successfully created",
-            user: {
-              user_id,
-              nickname,
-              comment,
-            },
-          };
-          return res.status(200).send(response);
-        });
-      } else {
+    }
+    
+    if (!result) {
+      let newUser = new Admin({
+        user_id,
+        password,
+        nickname,
+        comment,
+      });
+
+      Admin.createAdmin(newUser, (err, user) => {
+        if (err) throw err;
         const response = {
-          message: "Account creation failed",
-          cause: "already same user_id is used",
+          message: "Account successfully created",
+          user: {
+            user_id,
+            nickname,
+            comment,
+          },
         };
-        return res.status(400).send(response);
-      }
+        return res.status(200).send(response);
+      });
+    } else {
+      const response = {
+        message: "Account creation failed",
+        cause: "user_id already in use",
+      };
+      return res.status(400).send(response);
     }
   });
 };
@@ -75,6 +78,7 @@ const login = (req, res) => {
         user: user,
       });
     }
+    
     req.login(user, { session: false }, (err) => {
       if (err) {
         res.send(err);
@@ -97,8 +101,16 @@ const getAdminIdByEmail = (req, res) => {
     if (err) {
       return res.status(500).json({ success: false, error: err });
     }
+
+    if (!result) {
+      const response = {
+        message: "Email not found",
+      };
+      return res.status(404).send(response);
+    }
+
     return res.status(200).send({
-      success: false,
+      success: true,
       message: result,
     });
   });
@@ -106,6 +118,7 @@ const getAdminIdByEmail = (req, res) => {
 
 const authenticate = (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
+
   const jwtstatus = jwt.verify(token, "your_jwt_secret");
   passport.authenticate(
     "jwt",
@@ -118,6 +131,7 @@ const authenticate = (req, res, next) => {
       if (err) {
         return res.send(null);
       }
+
       if (info.message !== Constant.USER_DETECTED) {
         return res.send({ isLoggedIn: false });
       } else {
@@ -161,9 +175,9 @@ const getUserById = (req, res) => {
   const {
     params: { user_id: requested_user_id },
   } = req;
+  
   try {
-    const { decoded_user_id, decoded_password } =
-      decodeAuthorizationPayload(req);
+    const { decoded_user_id, decoded_password } = decodeAuthorizationPayload(req);
     req.body.user_id = decoded_user_id;
     req.body.password = decoded_password;
   } catch (error) {
@@ -172,6 +186,7 @@ const getUserById = (req, res) => {
     };
     return res.status(400).send(response);
   }
+
   const callbackFunction = () => getUserByIdAction(res, requested_user_id);
   authenticateCredentials(req, res, callbackFunction);
 };
@@ -181,6 +196,7 @@ const updateUserById = (req, res) => {
     params: { user_id: requested_user_id },
   } = req;
   const { nickname, comment } = req.body;
+
   if (!nickname && !comment) {
     const response = {
       message: "User updation failed",
@@ -188,19 +204,24 @@ const updateUserById = (req, res) => {
     };
     return res.status(400).send(response);
   }
+
   const payload = {
     nickname,
     comment,
   };
+
   const { decoded_user_id, decoded_password } = decodeAuthorizationPayload(req);
+
   if (requested_user_id !== decoded_user_id) {
     const response = {
       message: "No Permission for Update",
     };
     return res.status(403).send(response);
   }
+
   req.body.user_id = decoded_user_id;
   req.body.password = decoded_password;
+
   const callbackFunction = () =>
     updateUserByIdAction(res, requested_user_id, payload);
   authenticateCredentials(req, res, callbackFunction);
@@ -212,12 +233,14 @@ const getUserByIdAction = (res, requested_user_id) => {
     if (err) {
       return res.status(500).json({ success: false, error: err });
     }
+    
     if (!user) {
       const response = {
         message: "No User found",
       };
       return res.status(404).send(response);
     }
+
     const response = {
       message: "User details by user_id",
       user: {
@@ -234,15 +257,19 @@ const deleteUserById = (req, res) => {
   const {
     body: { user_id: requested_user_id },
   } = req;
+
   const { decoded_user_id, decoded_password } = decodeAuthorizationPayload(req);
+
   if (requested_user_id !== decoded_user_id) {
     const response = {
       message: "No Permission for Delete",
     };
     return res.status(403).send(response);
   }
+
   req.body.user_id = decoded_user_id;
   req.body.password = decoded_password;
+
   const callbackFunction = () => deleteUserByIdAction(res, requested_user_id);
   authenticateCredentials(req, res, callbackFunction);
 };
@@ -265,6 +292,7 @@ const updateUserByIdAction = (res, requested_user_id, payload) => {
         };
         return res.status(500).json(response);
       }
+
       const response = {
         message: "User successfully updated",
         user: {
@@ -285,6 +313,7 @@ const deleteUserByIdAction = (res, requested_user_id) => {
       };
       return res.status(500).json(response);
     }
+    
     const response = {
       message: "Account and User successfully removed",
     };
